bb1e1e8c8d442bcde8c4ff25e02d54d90cf18543
Weso Studio <wesostudio@gmail.com>
2024-09-14 12:42:38 -0300

    a

--- src/components/chapters/list-of-chapters.tsx
+++ src/components/chapters/list-of-chapters.tsx
@@ -1,11 +1,11 @@
+import ChapterItem from "./chapter";
+
+
 export default function ListOfChapters() {
     return (<section>
         <h2 className="p-4 font-bold text-3xl uppercase">Episodes</h2>
         <ul>
-            <ListOfChapters />
-            <ListOfChapters />
-            <ListOfChapters />
-            <ListOfChapters />
+            <ChapterItem />
         </ul>
     </section>)
 }
